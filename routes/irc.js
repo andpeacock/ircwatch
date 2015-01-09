62561e1bf7bf724c125ea710e1024851e60badfe
APeacock <apeacock@rirrgang-m1.roundarch.com>
2015-01-09 11:09:50 -0600

    Added moment more minor changes

--- routes/irc.js
+++ routes/irc.js
@@ -2,13 +2,14 @@ var irc= require('irc');
 var yo= require('./yo');
 var pb= require('./pastebin');
 var db= require('./db');
+var moment= require('moment');
 
 var client = new irc.Client('irc.zulusquad.org', 'FUCKBITCHESGETMONEY', {
   channels: ['#zulu']
 });
 client.addListener('message', function (from, to, message) {
   if(message.match('jezza(\d+)?')) {
-    var msgstr= from + ' => : ' + message;
+    var msgstr= '('+moment().format('MM/DD/YYYY HH:mm:ss')+'): '+from + ' => : ' + message;
     pb.newPaste(msgstr, yo.postYo);
     db.lpush('zulu', msgstr, function(reply) {
       console.log("worked?");
