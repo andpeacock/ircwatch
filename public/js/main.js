d7b9478a7a4f495cf0d5ecdbec099e5f3e100a59
Andrew Peacock <skooljester@gmail.com>
2015-01-21 09:21:26 -0600

    more style fixes

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,22 @@
 $(function() {
-  $('#rejoinZulu').on('click', function() {
-    $.get('/rejoin', function(data) {
-      alert("rejoined");
-    });
-  });
+  var main= {
+    init: function() {
+      var self= this;
+      self.binding();
+    },
+    binding: function() {
+      $('#rejoinZulu').on('click', function() {
+        $.get('/rejoin', function(data) {
+          alert("rejoined");
+        });
+      });
+      $('#imgurh1').on('click', function() {
+        if($('imgurImg').is(':visible'))
+          $('#imgurImg').slideUp(300);
+        else
+          $('#imgurImg').slideDown(300);
+      });
+    }
+  };
+  main.init();
 });
