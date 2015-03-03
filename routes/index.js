ed481c7073d59ba545e2c84256e99d22dfac4080
Andrew Peacock <skooljester@gmail.com>
2015-03-03 17:47:38 -0600

    Will this fix it??

--- routes/index.js
+++ routes/index.js
@@ -16,14 +16,20 @@ router.get('/', function (req, res) {
   //   });
   // });
   db.getMultiList(['imgur', 'zulu'], function (rlist) {
-    db.getTodo(function(todoRet) {
-      console.log(todoRet);
-      res.render('index', {
-        title: 'Random Shit',
-        linkList: rlist[0],
-        zuluList: rlist[1],
-        todo: todoRet
-      });
+    // db.getTodo(function(todoRet) {
+    //   console.log(todoRet);
+    //   res.render('index', {
+    //     title: 'Random Shit',
+    //     linkList: rlist[0],
+    //     zuluList: rlist[1],
+    //     todo: todoRet
+    //   });
+    // });
+    res.render('index', {
+      title: 'Random Shit',
+      linkList: rlist[0],
+      zuluList: rlist[1],
+      todo: ["A", "B", "C"]
     });
   });
 });
