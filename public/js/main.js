11b1332d70aa3685d836830b58e5cf735daa364d
Andrew Peacock <skooljester@gmail.com>
2015-03-03 18:14:49 -0600

    minor js tweaks

--- public/js/main.js
+++ public/js/main.js
@@ -12,16 +12,29 @@ $(function() {
         });
       });
       $('#imgurh1').on('click', function() {
-        if($('#imgurImg').is(':visible'))
-          $('#imgurImg').slideUp(300);
-        else
-          $('#imgurImg').slideDown(300);
+        headerClick('#imgurImg');
+        // if($('#imgurImg').is(':visible'))
+        //   $('#imgurImg').slideUp(300);
+        // else
+        //   $('#imgurImg').slideDown(300);
       });
+      $('#todoh1').on('click', function() {
+        headerClick('#todoList');
+      });
+      $('#zuluh1').on('click', function() {
+        headerClick('#zuluList');
+      });
+      function headerClick(bod) {
+        if($(bod).is(':visible'))
+          $(bod).slideUp(300);
+        else
+          $(bod).slideDown(300);
+      }
       $('.todoDel').on('click', function() {
         var tt= $(this).prev('p').text();
         console.log(tt);
         $.post('/todoDel', {todoText: tt}, function(data) {
-          console.log(data);
+          window.location(data);
         });
       });
     }
